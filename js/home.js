fb121ddd8f5282e4b27a0c2de30786d546a7f8ca
Julio Sousa Sender <julio.sender@neon.com.br>
2022-03-30 20:43:04 -0300

    modal sem ligacao API

--- js/home.js
+++ js/home.js
@@ -3,6 +3,26 @@ const optionsEvents = {
     method: "GET",
 }
 
+const myModal = document.querySelector("#myModal");
+const modalContent = document.querySelector("#modalContent");
+const closeBtn = document.querySelector("#closeBtn");
+const btnReserva = document.querySelector("#btnReserva");
+const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+const qtdIngressos = document.querySelector("#qtd");
+var aaa = "bbb";
+
+const eventoAbrirModal = (id) => {
+    const botao = document.querySelector(`#btn${id}`);
+    console.log(botao)
+    botao.addEventListener("click", (evento)=>{
+        myModal.style.display = "block";
+        console.log(botao.id.slice(3));
+        // window.location.href += "?id=aaaaaaaa"
+    });
+    
+}
+
 const ListarEventos = async (limit) => {
     const resposta = await fetch(`${BASE_URL}/events`, optionsEvents);
     const eventos = await resposta.json();
@@ -20,10 +40,41 @@ ListarEventos(3).then(resp => {
         <h2>${conteudo.name} - ${conteudo.scheduled}</h2>
         <h4>${conteudo.attractions}</h4>
         <p>${conteudo.description}</p>
-        <a href="#" class="btn btn-primary">reservar ingresso</a>
+        <a id="btn${conteudo._id}" class="btn btn-primary">reservar ingresso</a>
     </article>
         `
+    
+    });
+    resp.forEach(conteudo =>{
+        eventoAbrirModal(conteudo._id);
     });
 
-
 });
+
+closeBtn.addEventListener("click", (evento)=>{
+    evento.preventDefault();
+    myModal.style.display = "none";
+});
+
+window.onclick = function(event) {
+    if (event.target == myModal) {
+      myModal.style.display = "none";
+    }
+  }
+  
+
+btnReserva.addEventListener("click", (evento)=>{
+    evento.preventDefault();
+    const idEvento = "Hhhhhhh";
+    reserva = {
+        owner_name: nome.value,
+        owner_email: email.value,
+        number_tickets: qtdIngressos.value,
+        event_id: idEvento
+    };
+    console.log(reserva);
+    const reqOptions = {
+        method: 'POST',
+        body: reserva
+    };
+})
